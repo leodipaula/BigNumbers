a5bd3fb7f780aae196125d9d0bb308c2386bd6b4
Leonardo de Paula Fernandes <leofernandes9@gmail.com>
2023-12-13 13:02:56 -0300

    Agora roda em loop

--- client.c
+++ client.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 #include <strings.h>
 
-char *readDynamicLine(FILE *file)
+char *readDynamicLine()
 {
     char *line = NULL;
     size_t len = 0;
     int c;
     size_t index = 0;
 
-    while ((c = getchar()) != EOF && c != '\n')
+    while ((c = getchar()) != EOF && c != '\n' && c != ' ')
     {
         if (index == len)
         {
@@ -36,54 +36,50 @@ char *readDynamicLine(FILE *file)
     return line;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    if (argc < 2)
-    {
-        printf("Informe o nome do arquivo.\n");
-        return 1;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-
-    if (file == NULL)
-    {
-        printf("Problema ao abrir arquivo.\n");
-        return 1;
-    }
-
     BigNumber *num1 = createBigNumber();
     BigNumber *num2 = createBigNumber();
     BigNumber *result = createBigNumber();
     char operation, *strNum1, *strNum2;
 
-    strNum1 = readDynamicLine();
-    strNum2 = readDynamicLine();
-    scanf(" %c", &operation);
-
-    setBigNumberFromString(num1, strNum1);
-    setBigNumberFromString(num2, strNum2);
-
-    switch (operation)
+    while (1)
     {
-    case '+':
-        addBigNumbers(num1, num2, result);
-        break;
-    case '-':
-        subBigNumbers(num1, num2, result);
-        break;
-    case '*':
-        multiplyBigNumbers(num1, num2, result);
-        break;
-    default:
-        fprintf(stderr, "Operacao nao suportada: %c\n", operation);
-        break;
+        strNum1 = readDynamicLine();
+        if (strNum1 == NULL)
+            break;
+        strNum2 = readDynamicLine();
+        if (strNum2 == NULL)
+            break;
+        operation = getchar(); // read the operation
+        getchar();             // consume the newline
+
+        setBigNumberFromString(num1, strNum1);
+        setBigNumberFromString(num2, strNum2);
+
+        switch (operation)
+        {
+        case '+':
+            addBigNumbers(num1, num2, result);
+            break;
+        case '-':
+            subBigNumbers(num1, num2, result);
+            break;
+        case '*':
+            multiplyBigNumbers(num1, num2, result);
+            break;
+        default:
+            fprintf(stderr, "Operacao nao suportada: %c\n", operation);
+            break;
+        }
+
+        printf("%s\n", result->digits);
+        fflush(stdout);
+
+        free(strNum1);
+        free(strNum2);
     }
 
-    printf("%s\n", result->digits);
-
-    free(strNum1);
-    free(strNum2);
     destroyBigNumber(num1);
     destroyBigNumber(num2);
     destroyBigNumber(result);
